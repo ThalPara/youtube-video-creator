db511e6dc476e64f082deb05ac6cca7b2e0c7f98
Thalpara <thalparaproperties@gmail.com>
2025-08-10 11:32:57 +0100

    wtest

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'youtube-video-creator' // IMPORTANT for GitHub Pages
+  base: '/' // IMPORTANT for GitHub Pages
 })
